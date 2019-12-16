d6b4dfe855d332f81e15511dac8482fd8c9cc370
wenruo <wenruo@qq.com>
2019-12-17 03:32:29 +0800

    the first commit

---
+++ src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import MHeader from 'components/m-header/m-header'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'm-header',
+      component: MHeader
+    }
+  ]
+})
