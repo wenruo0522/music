ff6add64ecb5229aecadcd13fca635157f4daeeb
wenruo <wenruo@qq.com>
2019-12-24 00:51:36 +0800

    through the node proxy to get the api

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -9,6 +9,9 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 const portfinder = require('portfinder')
+// import axios
+const axios = require('axios')
+
 
 const HOST = process.env.HOST
 const PORT = process.env.PORT && Number(process.env.PORT)
@@ -42,6 +45,47 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     quiet: true, // necessary for FriendlyErrorsPlugin
     watchOptions: {
       poll: config.dev.poll,
+    },
+    // add the config 
+    before (app) {
+      app.get('/api/getTopBanner', function (req, res) {
+        const url = 'https://u.y.qq.com/cgi-bin/musicu.fcg'
+        const jumpPrefix = 'https://y.qq.com/n/yqq/album/'
+
+        axios.get(url, {
+          headers: {
+            referer: 'https://u.y.qq.com/',
+            host: 'u.y.qq.com'
+          },
+          params: req.query
+        }).then((response) => {
+          response = response.data
+          if (response.code === 0) {
+            const slider = []
+            const content = response.focus.data && response.focus.data.content
+            if (content) {
+              for (let i = 0; i < content.length; i++) {
+                const item = content[i]
+                const sliderItem = {}
+                sliderItem.id = item.id
+                sliderItem.linkUrl = jumpPrefix + item.jump_info.url + '.html'
+                sliderItem.picUrl = item.pic_info.url
+                slider.push(sliderItem)
+              }
+            }
+            res.json({
+              code: 0,
+              data: {
+                slider
+              }
+            })
+          } else {
+            res.json(response)
+          }
+        }).catch((e) => {
+          console.log(e)
+        })
+      })
     }
   },
   plugins: [
@@ -93,3 +137,6 @@ module.exports = new Promise((resolve, reject) => {
     }
   })
 })
+
+
+
--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,11 +1,10 @@
 import originJSONP from 'jsonp'
 
-export default function jsonp(url, data, option) {
+export default function jsonp (url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
-  
   return new Promise((resolve, reject) => {
     originJSONP(url, option, (err, data) => {
-      if(!err) {
+      if (!err) {
         resolve(data)
       } else {
         reject(err)
@@ -14,7 +13,7 @@ export default function jsonp(url, data, option) {
   })
 }
 
-function param(data) {
+function param (data) {
   let url = ''
   for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
