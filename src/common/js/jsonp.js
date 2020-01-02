ecfd9e3c1025c6fae7f06d60a2bf7b8c779b8a39
wenruo <wenruo@qq.com>
2020-01-03 02:02:05 +0800

    get the singer data

--- src/common/js/jsonp.js
+++ src/common/js/jsonp.js
@@ -1,9 +1,9 @@
-import originJSONP from 'jsonp'
+import originJsonp from 'jsonp'
 
 export default function jsonp (url, data, option) {
   url += (url.indexOf('?') < 0 ? '?' : '&') + param(data)
   return new Promise((resolve, reject) => {
-    originJSONP(url, option, (err, data) => {
+    originJsonp(url, option, (err, data) => {
       if (!err) {
         resolve(data)
       } else {
@@ -13,7 +13,7 @@ export default function jsonp (url, data, option) {
   })
 }
 
-function param (data) {
+export function param (data) {
   let url = ''
   for (var k in data) {
     let value = data[k] !== undefined ? data[k] : ''
