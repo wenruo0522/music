66baa019b91fdc2b9e18e9c5e4374302bafb498c
wenruo <wenruo@qq.com>
2020-01-03 16:05:54 +0800

    complete the data show

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="singer" ref="singer">
+    <listview :data="singers"></listview>
   </div>
 </template>
 
@@ -7,12 +8,16 @@
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
 import Singer from 'common/js/singer'
+import Listview from 'base/listview/listview'
 
 const HOT_NAME = '热门'
 const HOT_SINGER_LEN = 10
 
 export default {
   name: 'Singer',
+  components: {
+    Listview
+  },
   data () {
     return {
       singers: []
@@ -25,9 +30,7 @@ export default {
     _getSingerList () {
       getSingerList().then((res) => {
         if (res.code === ERR_OK) {
-          this.singers = res.data.list
-          console.log(this._normalizeSinger(this.singers))
-          // console.log(this.singers)
+          this.singers = this._normalizeSinger(res.data.list)
         }
       })
     },
