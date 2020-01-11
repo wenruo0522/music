36fe59999783a7af9089bcc201d43297016041a2
wenruo <wenruo@qq.com>
2020-01-11 14:02:55 +0800

    finish the search-save

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -16,7 +16,7 @@
       </div>
     </div>
     <div class="search-result" v-show="query">
-      <suggest :query="query" @listScroll="blurInput"></suggest>
+      <suggest :query="query" @listScroll="blurInput" @select="saveSearch"></suggest>
     </div>
     <router-view></router-view>
   </div>
@@ -27,6 +27,7 @@ import SearchBox from 'base/search-box/search-box'
 import { getHotKey } from 'api/search'
 import { ERR_OK } from 'api/config'
 import Suggest from 'components/suggest/suggest'
+import { mapActions } from 'vuex'
 
 export default {
   name: 'Search',
@@ -53,13 +54,19 @@ export default {
     blurInput () {
       this.$refs.searchBox.blur()
     },
+    saveSearch () {
+      this.saveSearchHistory(this.query)
+    },
     _getHotKey () {
       getHotKey().then((res) => {
         if (res.code === ERR_OK) {
           this.hotKey = res.data.hotkey.slice(0, 10)
         }
       })
-    }
+    },
+    ...mapActions([
+      'saveSearchHistory'
+    ])
   }
 }
 
